7f5f20c4959d1110e5d9a4e2b8a6bdbaa855b436
PROTEGE <protege@nssb.com.my>
2026-06-09 17:49:55 +0800

    Update Passport App

--- firebase.js
+++ firebase.js
@@ -1,35 +1,99 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  query,
-  where
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    doc,
+    query,
+    where,
+    onSnapshot,
+    orderBy
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    getStorage,
+    ref,
+    uploadString,
+    getDownloadURL
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCno2NNkzt3gLdC4CfZ4TlhwESQJgI_IXk",
-  authDomain: "test-login2-e4c7f.firebaseapp.com",
-  projectId: "test-login2-e4c7f",
-  storageBucket: "test-login2-e4c7f.firebasestorage.app",
-  messagingSenderId: "698181085162",
-  appId: "1:698181085162:web:3b91954eb1779a05894cd2"
+    apiKey: "AIzaSyCno2NNkzt3gLdC4CfZ4TlhwESQJgI_IXk",
+    authDomain: "test-login2-e4c7f.firebaseapp.com",
+    projectId: "test-login2-e4c7f",
+    storageBucket: "test-login2-e4c7f.firebasestorage.app",
+    messagingSenderId: "698181085162",
+    appId: "1:698181085162:web:3b91954eb1779a05894cd2"
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+    initializeApp(
+        firebaseConfig
+    );
 
-export const db = getFirestore(app);
+
+// =========================
+// FIRESTORE
+// =========================
+
+export const db =
+    getFirestore(app);
+
+
+// =========================
+// AUTH
+// =========================
+
+export const auth =
+    getAuth(app);
+
+
+// =========================
+// STORAGE
+// =========================
+
+export const storage =
+    getStorage(app);
+
+
+// =========================
+// EXPORTS
+// =========================
 
 export {
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  query,
-  where
+
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    doc,
+    query,
+    where,
+
+    onSnapshot,
+    orderBy,
+
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+
+    ref,
+    uploadString,
+    getDownloadURL
+
 };
